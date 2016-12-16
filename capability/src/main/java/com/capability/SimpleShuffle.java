16e9999e9a0caeb6e272d7b23f99fa8fbcd0a3e1
porali2000 <porali2000s@gmail.com>
2016-12-15 19:35:31 -0800

    Code  changed

--- capability/src/main/java/com/capability/SimpleShuffle.java
+++ capability/src/main/java/com/capability/SimpleShuffle.java
@@ -1,10 +1,14 @@
 package com.capability;
 
 import com.models.Deck;
+import com.models.PIP;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import javax.smartcardio.Card;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * Created by Porali_S on 12/15/2016.
@@ -12,8 +16,21 @@ import java.util.List;
 @Component
 @Profile("simple")
 public class SimpleShuffle implements Shuffle  {
+
+    Random random = new Random();
+
     @Override
     public List<Deck> shuffle(List<Deck> decks) {
-        return null;
+
+        decks.parallelStream()
+                .map(deck -> deck.getCards())
+                .collect(Collectors.toList()).forEach(deck -> {
+                swap(deck);
+        });
+        return collect;
+    }
+
+    private void swap(List<PIP> deck) {
+
     }
 }
